6c7aaee63326a5468c96c950230d1b82d4e3bf70
Ubaldo Andrea Desiato <36634403+theprototypedesigner@users.noreply.github.com>
2021-01-21 20:32:25 -0600

    New way to set parameters
    Intensity, frequency and duration can be set when declaring the object.

--- AdeHaptic.h
+++ AdeHaptic.h
@@ -7,28 +7,30 @@ class Haptic {
 
   private:
 
-    byte pin;
-    byte intensity;
-    int frequency;
-    int duration;
+    const byte _pin;
+    byte _intensity = 255;
+    short _frequency = 3000;
+    short _duration = 500;
     bool turnOn;
     bool isOff = true;
     unsigned long lastOffTime;
     unsigned long lastOnTime;
     
+    void init(byte intensity, short frequency, short duration);
 
   public:
 
     Haptic(byte pin);
+    Haptic(byte pin, byte intensity);
+    Haptic(byte pin, byte intensity, short frequency);
+    Haptic(byte pin, byte intensity, short frequency, short duration);
 
-    void init();
     void setIntensity(byte intensity);
     void setFrequency(float frequency);
-    void setDuration(int duration);
+    void setDuration(short duration);
     void on();
     void off();
     void update();
-
   
 };
 
--- examples/AdeHaptic_demo/AdeHaptic_demo.ino
+++ examples/AdeHaptic_demo/AdeHaptic_demo.ino
@@ -1,6 +1,7 @@
 #include "AdeHaptic.h"
 
-//initialize each motor, to use different intensity, use PWM pins. In this case I'm using pins 9,10 and 11 of an Arduino Uno
+// Declare each motor. To use different intensity, use PWM pins.
+// In this case I'm using pins 9,10 and 11 of an Arduino Uno.
 Haptic motor1(9);
 Haptic motor2(10);
 Haptic motor3(11);
@@ -13,30 +14,35 @@ byte intensity;
 
 void setup() {
 
-  //there are 10 different intensity, from 0 to 9, the default value is 9
+  //there are 10 different intensity, from 0 to 9. Default value is 9
   motor1.setIntensity(0);
 
-  //frequencies are set in Hertz and accept float values. For example, a value of 5 will result in 5 Hz meaning 1 buzz every 200 ms
+  //frequencies are set in Hertz and accept float values.
+  //For example, a value of 5 means 5 Hz that will result 1 buzz every 200 ms.
+  //Default value is 0 that means the motor is always on.
   motor2.setFrequency(1); //1 Hz -> 1 buzz every second
 
-  //Duration is how long the motor is on when buzzing. Default is 200 ms
+  //Duration is how long the motor is on when buzzing. Default value is 200 ms.
   //motor3 will wait for 200 ms and then vibrate for 1 second.
   motor3.setFrequency(5);
   motor3.setDuration(1000);
 
-  //turn on the 3 motors, to turn them off just use .off()
+  // It is also possible to set intensity, frequency and duration
+  //during the declaration of the motor. i.e. Haptic motor3(11,9,5,1000);
+
+  // Turn on the 3 motors, to turn them off just use .off()
   motor1.on();
   motor2.on();
   motor3.on();
 }
 
 void loop() {
-  //the update function for each motor has to be in the loop, this function is responsible of managing the motors
+  // The update function for each motor has to be in the loop, this function is responsible of managing the motors
   motor1.update();
   motor2.update();
   motor3.update();
 
-  // every 2 seconds increase the intensity of motor 1
+  // Every 2 seconds increase the intensity of motor 1
   if (millis() - lastIntensityTime > 2000) {
     lastIntensityTime = millis();
     intensity++;
@@ -44,12 +50,11 @@ void loop() {
     motor1.setIntensity(intensity);
   }
 
-  // every 5 seconds increase the frequency of motor 2
-  if ((millis() - lastHzTime) > 5000) {
+  // Every 5 seconds increase the frequency of motor 2
+  if (millis() - lastHzTime > 5000) {
     lastHzTime = millis();
     Hz++;
     if (Hz > 4) Hz = 0;
     motor2.setFrequency(frequency[Hz]);
   }
-  
 }
